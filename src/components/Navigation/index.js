f324b885ed31af3e0bda477794d1abb9f1a72b6f
kevinbett <bettkevin757@gmail.com>
2018-07-07 18:53:48 +0300

    [Chore] Updates home button

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import api from '../../api'
 const Navigation = () => (
 
   <div className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">Home</Link>
+    <Link className="navbar-brand" to="/viewbusiness">Home</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
