78076c5cc4352ec099881291fe770f2dfa010804
kevinbett <bettkevin757@gmail.com>
2018-07-10 16:13:23 +0300

    [Feature #158905859] Adds Logintest

---
+++ src/components/test/LoginForm.test.js
@@ -0,0 +1,8 @@
+import LoginForm from "../LoginForm/LoginForm"
+import { shallow } from "enzyme"
+
+describe('login page renders',() => { 
+    it ('populates the the login form', () => { 
+        expect(shallow(<LoginForm/>).find("div").exists()).toBe(true);
+    })
+} )
