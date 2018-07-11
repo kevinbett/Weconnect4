81e11e74d9633985ffe76cc3d2c1b84634a83fd8
kevinbett <bettkevin757@gmail.com>
2018-07-11 11:01:27 +0300

    [Feature #158905859] Implements tests. - Adds Login tests - Adds Business Registration test. - Adds Navigation bar test. - Adds new password form test.

---
+++ src/components/test/businessregistration.test.js
@@ -0,0 +1,9 @@
+import RegisterBusinessForm from "../RegisterBusinessForm"
+import { shallow } from "enzyme"
+import { Divider } from "../../../node_modules/semantic-ui-react";
+
+describe('register business form',() => { 
+    it ('renders business registration form', () => { 
+        expect(shallow(<RegisterBusinessForm/>).find("div#register").exists()).toBe(true);
+    })
+} )
