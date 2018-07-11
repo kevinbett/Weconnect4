4be08fdf775f46cc27b1711331e7cd7d7e40752e
kevinbett <bettkevin757@gmail.com>
2018-07-11 11:40:45 +0300

    [Feature #158905859] Updates reset password tests

--- src/components/test/resetpassword.test.js
+++ src/components/test/resetpassword.test.js
@@ -3,13 +3,6 @@ import { shallow } from "enzyme"
 
 describe('registration page renders',() => { 
     it ('populates the signup form', () => { 
-        expect(shallow(<ResetPassword/>).find("input.button").exists()).toBe(true),
-        expect(shallow(<ResetPassword/>).find("div#main-registration-container").exists()).toBe(true),
-        expect(shallow(<ResetPassword/>).find("form").exists()).toBe(true),
-        expect(shallow(<ResetPassword/>).find("input").exists()).toBe(true),
-        expect(shallow(<ResetPassword/>).find("label").exists()).toBe(true),
-        expect(shallow(<ResetPassword/>).find("p").exists()).toBe(true)
-        
-        ;
-    })
+        expect(shallow(<ResetPassword/>)).toMatchSnapshot()
+    });
 } )
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import { createSerializer } from 'enzyme-to-json';
+import { createSerializer } from 'enzyme-to-json';
 // import sinon from 'sinon';
 // snapshot serailizer for producing more human readable snapshots
-// expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 Enzyme.configure({ adapter: new Adapter() });
 // mock localStorage
 class LocalStorageMock {
